ebd3219be55a60d9d8896fd1de71db8c917d7e62
hudsonviana <hudson1206@gmail.com>
2025-02-16 13:28:45 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-blue-500 underline text-5xl font-bold">
+      Teste Tailwind
+    </div>
+  )
+}
+
+export default App
